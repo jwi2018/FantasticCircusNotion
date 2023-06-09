12a9b2b232f9a9247502d346c1c8063dd64ad69c
jwi <jwi2018@naver.com>
2023-06-09 09:50:41 +0900

    프로젝트 이동

---
+++ Assets/07. Settings/StaticGameSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public static class StaticGameSettings
+{
+    public static bool IsJewelsAdventure = true;
+    public static int TotalStage = 1800;
+    public static bool StageOpenDebug = false;
+    
+    public static bool IsAbleFish = true;
+
+    public static string CsReportTitle = "Fantastic Circus Match 3 Puzzle Cs Report";
+
+    public static int TestPlusDay = 0;
+
+    public static int iLimitStageEventLevel = 24;
+
+    public static string ReviewAOSURL = "market://details?id=com.icecube.fantasticcircus.android.free.puzzle";
+    public static string ReviewIOSURL = "여기 리뷰 링크";
+
+    public static float BounceValue = 0.2f;
+
+    public static string AOSBannerKey = "dd3cc020c3bc98e9";
+    public static string AOSInterstitialKey = "357de3bb36745fc2";
+    public static string AOSRewardKey = "39cec08f5e3f38ae";
+
+    public static string IOSBannerKey = "dd3cc020c3bc98e9";
+    public static string IOSInterstitialKey = "357de3bb36745fc2";
+    public static string IOSRewardKey = "39cec08f5e3f38ae";
+
+    public static string AOSNativeKey = "1";
+    public static string IOSNativeKey = "1";
+
+    public static bool IsUseDoubleClick = true;
+}
